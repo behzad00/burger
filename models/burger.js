f5c0fd36347f8c47568bbef81fa0f5270fa1b75a
behzad00 <behzadpouyanfar@gmail.com>
2017-05-15 23:13:00 -0400

    mon4

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+var burger = {
+	all : function(cb){
+		orm.all("burgers", function(res){
+			cb(res);
+		});
+	};
+	create: function(cols, vals, cb){
+		orm.create("burgers", cols, vals, function(res){
+			cb(res);
+		});
+
+	};
+	update: function(objColVals, condition, cb){
+		orm.update("burgers", objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+	delete: function(condition, cb){
+		orm.delete("burgers", condition, function(res){
+			cb(res);
+		});
+	}
+};
+
+module.exports = burger;
